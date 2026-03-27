20dc11ff04be1aed717caec5b11e0ee5af0af246
ZCNdlovu <211204803@mycput.ac.za>
2026-03-27 22:20:25 +0200

    Fixed Small bugs for entle-myezo

--- src/main/java/za/ac/cput/repository/IBookingConfirmationRepository.java
+++ src/main/java/za/ac/cput/repository/IBookingConfirmationRepository.java
@@ -3,6 +3,8 @@ package za.ac.cput.repository;
 import za.ac.cput.domain.BookingConfirmation;
 
 public interface IBookingConfirmationRepository extends IRepository<BookingConfirmation, String> {
+    BookingConfirmation findById(String id);
+
     BookingConfirmation findByBookingReference(String bookingReference);
     BookingConfirmation findByConfirmationNumber(String confirmationNumber);
 }
